923a16e062f565dbc35099b6568b9d35283b21a8
Jhojancn <N00447986@upn.pe>
2026-09-21 18:56:31 -0500

    Inicialización del proyecto

---
+++ src/jhojan_t1_tecnicas/Jhojan_T1_Tecnicas.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jhojan_t1_tecnicas;
+
+/**
+ *
+ * @author UCA40406
+ */
+public class Jhojan_T1_Tecnicas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
